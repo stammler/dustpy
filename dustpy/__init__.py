18fcbe9c45c75015be8ad56585832747452a90e7
Sebastian Stammler <sebastian.stammler@gmail.com>
2020-11-09 15:23:48 +0100

    Change version number to 0.4.7

--- dustpy/__init__.py
+++ dustpy/__init__.py
@@ -21,7 +21,7 @@ from simframe.io.dump import readdump
 from simframe.io.writers import hdf5writer
 
 __name__ = "dustpy"
-__version__ = "0.4.6"
+__version__ = "0.4.7"
 
 Simulation.__version__ = __version__
 plot.__version__ = __version__
